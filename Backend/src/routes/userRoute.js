a8598574e682dcf83457aec0e8cb2ad41936d48f
Vinit Upadhyay <91243407+vinit84@users.noreply.github.com>
2024-02-23 12:24:35 +0530

    deploy

--- Backend/src/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const userController = require("../controller/userController.js");
-
-router.get("/profile", userController.getUserProfile);
-router.get("/", userController.getAllUsers);
-router.delete("/:userId", userController.deleteUser);
-module.exports = router;
